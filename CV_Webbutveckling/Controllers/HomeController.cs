379149579e923ab6f3f9465d955b221f281cb3e1
sharpcad <dmitri.potchepko@gmail.com>
2022-01-16 21:47:27 +0100

    Add project files.

---
+++ CV_Webbutveckling/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CV_Webbutveckling.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CV_Webbutveckling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult WorkExperience()
+        {
+            return View();
+        }
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
